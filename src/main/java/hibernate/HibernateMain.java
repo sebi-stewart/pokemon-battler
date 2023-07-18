8d63a39e67b32717cc9d3559582fc3363d739bce
sebi_stewart <sebastianstewart03@gmail.com>
2023-07-18 18:54:44 +0200

    Added two hibernate functions to each create a Pokemon or a Move in the Database

--- src/main/java/hibernate/HibernateMain.java
+++ src/main/java/hibernate/HibernateMain.java
@@ -5,6 +5,8 @@ import org.hibernate.Session;
 
 import hardcoded.*;
 
+import java.util.Set;
+
 /**
  * This class contains the functions to access and modify the database
  * @author Sebastian Stewart
@@ -28,7 +30,52 @@ public class HibernateMain {
         }
     }
 
-    public static Pokemon createPokemon(){
-        return null;
+    public static Pokemon createPokemon(int pokemonID, String pokemonName, Type[] types, int[] baseStats, Set<Move> moves){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Pokemon p1 = new Pokemon();
+        try {
+            session.beginTransaction();
+            p1.setPokemonID(pokemonID);
+            p1.setName(pokemonName);
+            p1.setTypes(types);
+            p1.setBaseStats(baseStats);
+            p1.setMoves(moves);
+
+            session.save(p1);
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            if(session!=null) session.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            assert session != null;
+            session.close();
+        }
+        return p1;
+
+    }
+
+    public static Move createMove (int moveID, String name, int power, int accuracy, Type type, MoveCategory category){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Move m1 = new Move();
+        try {
+            session.beginTransaction();
+            m1.setMoveID(moveID);
+            m1.setName(name);
+            m1.setPower(power);
+            m1.setAccuracy(accuracy);
+            m1.setType(type);
+            m1.setCategory(category);
+
+            session.save(m1);
+            session.getTransaction().commit();
+
+        } catch (HibernateException e) {
+            if(session!=null) session.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            assert session != null;
+            session.close();
+        }
+        return m1;
     }
 }
