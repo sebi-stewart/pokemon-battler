ad211ac993b7dcf6aef673ae46f9860248b6d1b5
sebi_stewart <sebastianstewart03@gmail.com>
2023-07-22 21:46:34 +0200

    Added a randomizer function to get a random nature for the party pokemon

--- src/main/java/party/Nature.java
+++ src/main/java/party/Nature.java
@@ -1,9 +1,19 @@
 package party;
 
+import java.util.Random;
+
 public enum Nature {
     HARDY, LONELY, ADAMANT, NAUGHTY, BRAVE,
     BOLD, DOCILE, IMPISH, LAX, RELAXED,
     MODEST, MILD, BASHFUL, RASH, QUIET,
     CALM, GENTLE, CAREFUL, QUIRKY, SASSY,
-    TIMID, HASTY, JOLLY, NAIVE, SERIOUS
+    TIMID, HASTY, JOLLY, NAIVE, SERIOUS;
+
+    private static final Nature[] VALUES = values();
+    private static final int SIZE = VALUES.length;
+    private static final Random RANDOM = new Random();
+
+    public static Nature getRandomNature()  {
+        return VALUES[RANDOM.nextInt(SIZE)];
+    }
 }
