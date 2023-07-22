91974c10d8b17bed3617c14856a2e00cb3de593d
sebi_stewart <sebastianstewart03@gmail.com>
2023-07-22 21:47:01 +0200

    Added getters and setter plus a constructer to this document

--- src/main/java/party/PartyMon.java
+++ src/main/java/party/PartyMon.java
@@ -15,7 +15,7 @@ public class PartyMon{
 
     @ManyToOne
     @JoinColumn(name="pokemon_id", nullable = false)
-    private Pokemon pokemonID;
+    private Pokemon partyPokemon;
 
     @Column(name = "level")
     private byte level;
@@ -26,7 +26,7 @@ public class PartyMon{
 
     @Enumerated(EnumType.STRING)
     @Column(name = "status_effect")
-    private Status status_effect;
+    private Status status_effect=null;
 
     @Column(name = "total_health")
     private short total_health;
@@ -49,23 +49,161 @@ public class PartyMon{
     @Column(name = "current_health")
     private short currentHealth;
 
+    public int getPartyMonID() {
+        return partyMonID;
+    }
+
+    public void setPartyMonID(int partyMonID) {
+        this.partyMonID = partyMonID;
+    }
+
+    public Pokemon getPartyPokemon() {
+        return partyPokemon;
+    }
+
+    public void setPartyPokemon(Pokemon pokemonID) {
+        this.partyPokemon = pokemonID;
+    }
+
+    public byte getLevel() {
+        return level;
+    }
+
+    public void setLevel(byte level) {
+        this.level = level;
+    }
+
+    public Nature getNature() {
+        return nature;
+    }
+
+    public void setNature(Nature nature) {
+        this.nature = nature;
+    }
+
+    public Status getStatus_effect() {
+        return status_effect;
+    }
+
+    public void setStatus_effect(Status status_effect) {
+        this.status_effect = status_effect;
+    }
+
+    public short getTotal_health() {
+        return total_health;
+    }
+
+    public void setTotal_health(short total_health) {
+        this.total_health = total_health;
+    }
+
+    public short getTotal_atk() {
+        return total_atk;
+    }
+
+    public void setTotal_atk(short total_atk) {
+        this.total_atk = total_atk;
+    }
+
+    public short getTotal_def() {
+        return total_def;
+    }
+
+    public void setTotal_def(short total_def) {
+        this.total_def = total_def;
+    }
+
+    public short getTotal_spa() {
+        return total_spa;
+    }
+
+    public void setTotal_spa(short total_spa) {
+        this.total_spa = total_spa;
+    }
+
+    public short getTotal_spd() {
+        return total_spd;
+    }
+
+    public void setTotal_spd(short total_spd) {
+        this.total_spd = total_spd;
+    }
+
+    public short getTotal_spe() {
+        return total_spe;
+    }
+
+    public void setTotal_spe(short total_spe) {
+        this.total_spe = total_spe;
+    }
+
+    public short getCurrentHealth() {
+        return currentHealth;
+    }
+
+    public void setCurrentHealth(short currentHealth) {
+        this.currentHealth = currentHealth;
+    }
+
     public PartyMon() {
     }
 
-    public PartyMon(Pokemon pokemonID) {
+    public PartyMon(Pokemon partyPokemon, int level) {
         this.partyMonID = 1;
-        this.pokemonID = pokemonID;
+        this.partyPokemon = partyPokemon;
+        this.level = (byte) level;
+        setTotals();
+        this.currentHealth = total_health;
     }
 
-    private void setTotals(){
-        short[] baseStats = pokemonID.getBaseStats();
+    public void setTotals(){
+        short[] baseStats = partyPokemon.getBaseStats();
         this.total_health = (short)((short)(((float)(2*baseStats[0])*level)/100) + level + 10);
-        this.total_atk = calcBeforeNature(baseStats[1], 0, 0)
+        this.total_atk = (short)Math.floor(calcBeforeNature(baseStats[1], 0, 0) * getNatureMultiplier(1));
+        this.total_def = (short)Math.floor(calcBeforeNature(baseStats[2], 0, 0) * getNatureMultiplier(2));
+        this.total_spa = (short)Math.floor(calcBeforeNature(baseStats[3], 0, 0) * getNatureMultiplier(3));
+        this.total_spd = (short)Math.floor(calcBeforeNature(baseStats[4], 0, 0) * getNatureMultiplier(4));
+        this.total_spe = (short)Math.floor(calcBeforeNature(baseStats[5], 0, 0) * getNatureMultiplier(5));
+
     }
 
     private float calcBeforeNature(int baseStat, int IV, float EV){
-        float topFraction = (2*baseStat + IV + EV/4);
-        float centreBracket = (float)Math.floor(topFraction/100) + 5;
-        return centreBracket;
+        float topFraction = (2*baseStat + IV + EV/4)*level;
+        return (float)Math.floor(topFraction/100) + 5;
+    }
+
+    private float getNatureMultiplier(int index){
+        return switch(index){
+            case 1: yield switch(nature) {
+                case BOLD, MODEST, CALM, TIMID: yield 0.9f;
+                case LONELY, ADAMANT, NAUGHTY, BRAVE: yield 1.1f;
+                default: yield 1.0f;};
+            case 2: yield switch(nature) {
+                case LONELY, MILD, GENTLE, HASTY: yield 0.9f;
+                case BOLD, IMPISH, LAX, RELAXED: yield 1.1f;
+                default: yield 1.0f;};
+            case 3: yield switch(nature) {
+                case ADAMANT, IMPISH, CAREFUL, JOLLY: yield 0.9f;
+                case MODEST, MILD, RASH, QUIET: yield 1.1f;
+                default: yield 1.0f;};
+            case 4: yield switch(nature) {
+                case NAUGHTY, LAX, RASH, NAIVE: yield 0.9f;
+                case CALM, GENTLE, CAREFUL, SASSY: yield 1.1f;
+                default: yield 1.0f;};
+            case 5: yield switch(nature) {
+                case BRAVE, RELAXED, QUIET, SASSY: yield 0.9f;
+                case TIMID, HASTY, JOLLY, NAIVE: yield 1.1f;
+                default: yield 1.0f;};
+            default: yield 1.0f;
+        };
+    }
+
+    @Override
+    public String toString() {
+        return "PartyMon{" +
+                "partyMonID=" + partyMonID +
+                ", pokemonID=" + partyPokemon +
+                ", level=" + level +
+                '}';
     }
 }
